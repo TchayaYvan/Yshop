948b74f3f6bf57d02a75b828f29fe1ed99b621ab
Tchaya Yvan <tchayayvan76@gmail.com>
2023-04-08 21:41:58 +0200

    React-Node

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+       <a href="/">Yshop</a>
+      </header>
+      <main>
+        List products
+      </main>
+    </div>
+  );
+}
+
+export default App;
